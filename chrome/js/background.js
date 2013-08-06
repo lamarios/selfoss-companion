1e853b0535d3b002bac189d0b5e282334d6e623b
Paul Fauchon <p.fauchon@gmail.com>
2013-08-06 21:45:50 +0800

    Initial commit with website and chrome extension

---
+++ chrome/js/background.js
@@ -0,0 +1,205 @@
+var unreadCount = 0;
+var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+
+$(document).ready(function(){
+
+	checkUnread();
+	
+	startUnreadAlarm();
+	
+	chrome.browserAction.onClicked.addListener(visitSite);
+	
+	chrome.notifications.onClicked.addListener(visitSite);
+	
+	chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+		console.log("[MESSAGE] "+request.message);
+		
+		if (request.message == "check") {
+			checkUnread();    
+		}
+		
+		if (request.message == "startUpdateAlarm") {
+			startUpdateAlarm();    
+		}
+		
+		if (request.message == "stopUpdateAlarm") {
+			stopUpdateAlarm();    
+		}
+		
+		if (request.message == "restartUpdateAlarm") {
+			stopUpdateAlarm();    
+			startUpdateAlarm();    
+		}
+	});
+	
+	//checking if we need to start the update alarm
+	 chrome.storage.local.get('update', function(data) {
+      if (data.update){
+      	startUpdateAlarm();
+      }
+    });
+});
+
+
+/*
+* Starts the unread alarm that will regularly check the unread count
+*/
+function startUnreadAlarm(){
+
+	console.log("[UNREAD UPDATE] Creating alarm");
+	var alarm = chrome.alarms.create("unreadAlarm", {
+		when:0,
+		periodInMinutes: 10
+	});
+	
+	console.log("[ALARMS] Adding event to alarm");
+	chrome.alarms.onAlarm.addListener(function(alarm) {
+		if(alarm.name == "unreadAlarm"){
+			checkUnread();
+		}
+		
+		if(alarm.name == "updateAlarm"){
+			updateFeeds();
+		}
+	});
+	listAlarms();
+}
+
+
+/*
+* Ajax call to the stats URL of the selfoss installation
+*/
+function checkUnread(){
+	console.log("[UNREAD UPDATE]  Getting unread count");
+	chrome.storage.local.get('url', function(data) {
+    	if (data.url && data.url.match(regex)){
+    		console.log("[UNREAD UPDATE]  Calling url"+data.url+"/stats");
+	        $.getJSON(data.url+"/stats", updateCounter).fail(failUnread);
+    	}else{
+	    	console.log("[UNREAD UPDATE]  Invalid url");
+    		chrome.browserAction.setBadgeText({text: "!"});
+    	}
+    });
+}
+
+
+/*
+* Updated the badge counter
+*/
+function updateCounter(result){
+	console.log("[UNREAD UPDATE] parsing result");
+	if(result.unread > 0){
+		chrome.browserAction.setBadgeText({text: result.unread});
+		
+		if(unreadCount != result.unread){
+			//Getting old unread count, if different than new one, display notification
+    		chrome.notifications.create("unreadCount", {
+    		  type: "basic",
+			  iconUrl:"logo.png",  // icon url - can be relative
+			  title:'Selfoss',  // notification title
+			  message:'You have '+result.unread+" unread items.",  // notification body text
+			  }
+			  ,function(){}
+			);
+		}
+	}
+	
+	unreadCount = result.unread;
+}
+
+
+/*
+* Happens when the feed update fails
+*/
+function failUpdate(){
+	console.log("[UPDATE TRIGGER] Fail to update feeds");
+	chrome.browserAction.setBadgeText({text: "!"});
+}
+
+
+/*
+* Happens when the unread count fails
+*/
+function failUnread(){
+	console.log("[UNREAD UPDATE] Fail to get unread count");
+	chrome.browserAction.setBadgeText({text: "!"});
+}
+
+
+/*
+* Opens a new tab and opens the selfoss URL
+*/
+function visitSite(tab){
+	chrome.storage.local.get('url', function(data) {
+    	if (data.url && data.url.match(regex)){
+			chrome.tabs.create({url: data.url});
+		}else{
+			chrome.tabs.create({url: "settings.html"});
+		}
+	});
+}
+
+
+/*
+* Starts the update feeds alarm based on the timer in the preferences
+*/
+function startUpdateAlarm(timer){
+	if(timer == undefined){ // if no timer passed in param, we fetch it
+		console.log("[UPDATE TRIGGER] Starting alarm");
+		chrome.storage.local.get('timer', function(data) {
+      	if (!isNaN(data.timer)){
+        	startUpdateAlarm(data.timer);
+      	}
+    });
+
+	}else{
+		console.log("[UPDATE TRIGGER] Creating update alarm timer: "+timer);
+		var alarm = chrome.alarms.create("updateAlarm", {
+			when:0,
+			periodInMinutes: parseInt(timer)
+		});
+			listAlarms();
+	}
+}
+
+
+/*
+* Stops the update alarm
+*/
+function stopUpdateAlarm(){
+	console.log("[ALARMS] Getting feed update alarm");
+	chrome.alarms.clear("updateAlarm");
+	listAlarms();
+}
+
+
+/*
+* Ajax call to the feed update url.
+*/
+function updateFeeds(){
+	console.log("[UPDATE TRIGGER] updating feeds");
+	
+	chrome.storage.local.get('url', function(data) {
+    	if (data.url && data.url.match(regex)){
+    		console.log("[UPDATE TRIGGER]  Calling url "+data.url+"/update");
+	        $.get(data.url+"/update", function(){
+	        	console.log("[UPDATE TRIGGER] Feed Updating finished");
+	        	checkUnread();
+	        }).fail(failUpdate);
+    	}else{
+	    	console.log("[UNREAD UPDATE]  Invalid url");
+    	}
+	});
+}
+
+/*
+* Lists down the currently running alarms, for logging purpose
+*/
+function listAlarms(){
+	chrome.alarms.getAll(function (alarms){
+		$.each(alarms, function(index, alarm){
+			console.log("[ALARM LIST]"+alarm.name);
+		});
+	});
+}
---
+++ chrome/js/settings.js
@@ -0,0 +1,139 @@
+var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+var update;
+var oldTimer = 20;
+
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+    return this.slice(-str.length) == str;
+  };
+}
+
+
+$(document).ready(function(){
+	
+
+	$("#settings").submit(saveChanges);
+	
+	$("#update").click(toggleTimer);
+	
+	console.log("Trying to get settings");
+	chrome.storage.local.get('url', function(data) {
+      if (data.url)
+        $("#url").val(data.url);
+    });
+    
+    chrome.storage.local.get('update', function(data) {
+      if (data.update){
+      	$("#update").prop("checked", data.update);
+      	update = data.update;
+      	
+      }
+      toggleTimer();
+    });
+    
+    
+    chrome.storage.local.get('timer', function(data) {
+      if (!isNaN(data.timer)){
+        $("#timer").val(data.timer);
+        oldTimer = data.timer;
+      }else{
+     	$("#timer").val(20);
+      }
+    
+    });
+  
+});
+
+
+/*
+* Save changes made to the settings
+*/
+function saveChanges() {
+
+	// Get a value saved in a form.
+	var url = $("#url").val();
+	var timer = $("#timer").val();
+	var newUpdate = $("#update").prop("checked");
+	
+	
+	if(!url.startsWith("http://") && !url.startsWith("https://") ){
+		url = "http://"+url;
+	}
+	
+	if(url.endsWith("/")){
+		url = url.slice(0, - 1);
+	}
+	/*
+	var username = $("#username").val();
+	var password = $("#password").val();
+	*/
+	// Check that there's some code there.
+	if (!url) {
+		alert('Error: No URL specified');
+		return false;
+	}
+	
+	if(!url.match(regex)){
+		alert("Invalid URL");
+		return false;
+	}
+	
+	if(isNaN(timer)){
+		alert("The timer must be a number");
+		return false;
+	}
+	
+	//alert(url);
+	// Save it using the Chrome extension storage API.
+	chrome.storage.local.set({
+		'url': url,
+		'update': newUpdate,
+		'timer': timer
+		//'username': username,
+		//'password': password
+		}, function() {
+		alert('Settings saved');
+	});
+	
+	chrome.extension.sendMessage({message: "check"}, function(response) {
+
+	});
+	
+	//only if the update has been enabled, we trigger the alarm
+	if(newUpdate && !update){
+		chrome.extension.sendMessage({message: "startUpdateAlarm"});
+	}
+	
+	//Stops the alarm if it was running previously
+	if(!newUpdate && update){
+		chrome.extension.sendMessage({message: "stopUpdateAlarm"});
+	}
+	
+	//If w the timer is changed, the alarm has to be restarted
+	if(update && timer != oldTimer){
+		chrome.extension.sendMessage({message: "restartUpdateAlarm"});
+	}
+	
+	update = newUpdate;
+	return false;
+}
+
+
+/*
+* Slide up or down the timer input
+*/
+function toggleTimer(){
+	if($("#update").prop("checked")){
+		$("#timer-container").slideDown();
+	}else{
+		$("#timer-container").slideUp();
+	}
+}
+
